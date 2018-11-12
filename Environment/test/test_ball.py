7008291c4608dcf888fc765e56f5ce6f450812b4
James Rodney <jlrodney95@gmail.com>
2018-11-12 16:36:56 +0000

    Tested check collisions method
    - To show that velocity changes direction when it hits a wall and maintains its direction when it does not

--- Environment/test/test_ball.py
+++ Environment/test/test_ball.py
@@ -7,6 +7,10 @@ class BallTest(unittest.TestCase):
     def setUp(self):
         self.ball = Ball(20, 20, 1, 1, 10, 600, 900)
 
-    def testStep(self):
+    def test_step(self):
         self.ball.step()
         self.assertTrue((self.ball.position == np.array([21, 21])).all())
+
+    def test_check_collisions(self):
+        self.ball.check_collisions()
+        self.assertTrue((self.ball.velocity == np.array([1, 1])).all())
