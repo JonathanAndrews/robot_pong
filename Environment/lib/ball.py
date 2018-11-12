8ed3e76dbd8fe2acef8c43e7ee690dd580afe542
Cameron Whitehead <camjw119@gmail.com>
2018-11-12 15:59:15 +0000

    Added ball class
    Why:
    Starting creating python frame of game
    *
    
    This change addresses the need by:
    Creating the first class
    *

---
+++ Environment/lib/ball.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Ball:
+    def __init__(self, position_x, position_y, velocity_x, velocity_y, radius, canvas_width, canvas_height):
+        self.position = np.array([position_x, position_y])
+        self.velocity = np.array([velocity_x, velocity_y])
+        self.radius = radius
+        self.canvas = np.array([canvas_width, canvas_height])
+
+    def step(self):
+        self.check_collisions()
+        self.position += self.velocity
+
+    def check_collisions(self):
+        for i in [0,1]:
+            if (self.position[i] + self.radius >= self.canvas[i]) or (
+                self.position[i] - self.radius =< 0):
+                self.velocity[i] *= -1
