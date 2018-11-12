bfcf3fee4585501ac3495d6ed0cceacf3906b5d7
NadiaAiraf <aidanfaria@gmail.com>
2018-11-12 16:39:02 +0000

    fully tested draw function

--- spec/paddle.test.js
+++ spec/paddle.test.js
@@ -9,7 +9,6 @@ describe('Paddle', function() {
     stubContext = {
       beginPath: jest.fn(),
       rect: jest.fn(),
-      fillStyle: jest.fn(),
       fill: jest.fn(),
       closePath: jest.fn()
     };
@@ -17,10 +16,14 @@ describe('Paddle', function() {
       getContext: jest.fn(() => stubContext)
     };
   })
-  
+
   it('calls beginPath on ctx', () => {
     const paddle = new Paddle(stubCanvas);
     paddle.draw()
     expect(stubContext.beginPath).toHaveBeenCalledTimes(1);
+    expect(stubContext.rect).toHaveBeenCalledTimes(1);
+    expect(stubContext.fill).toHaveBeenCalledTimes(1);
+    expect(stubContext.closePath).toHaveBeenCalledTimes(1);
+    expect(stubContext.fillStyle).toEqual('#FFFFFF')
   })
 })
