adc0496a56590bf97f571e55e92dc4344d974d6c
NadiaAiraf <aidanfaria@gmail.com>
2018-11-12 16:53:27 +0000

    Test for pongGame calling the run function

--- spec/game.test.js
+++ spec/game.test.js
@@ -1,8 +1,17 @@
-const game = require('../src/game')
+const Game = require('../src/game')
 
 describe('Game', function() {
-  
-  it('calls draw on the paddle object', () => {
+  let pongGame
 
+  beforeEach(() => {
+    stubPaddle = {
+      draw: jest.fn()
+    };
+    pongGame = new Game(stubPaddle);
+  })
+
+  test('calls draw on the paddle object', () => {
+    pongGame.run()
+    expect(pongGame.paddle.draw).toHaveBeenCalledTimes(1)
   })
 })
--- src/game.js
+++ src/game.js
@@ -1,10 +1,9 @@
-function Game() {
-  this.paddle = new Paddle(document.getElementById('myCanvas'));
+var Game = function(paddle = new Paddle(document.getElementById('myCanvas'))) {
+  this.paddle = paddle;
 }
 
 Game.prototype.run = function () {
-
   this.paddle.draw()
-
-
 }
+
+module.exports = Game
