60caba03c74a464732e1fe9f9f1426aba151d2a2
Cameron Whitehead <camjw119@gmail.com>
2018-11-13 14:49:24 +0000

    Added ball/paddle collision
    Why:
    Game logic
    *
    
    This change addresses the need by:
    Adding left and right paddle collision functions
    *

--- Environment/lib/ball.py
+++ Environment/lib/ball.py
@@ -2,15 +2,8 @@ import numpy as np
 
 
 class Ball:
-    def __init__(
-            self,
-            position_x,
-            position_y,
-            velocity_x,
-            velocity_y,
-            radius=5,
-            canvas_width=900,
-            canvas_height=600):
+    def __init__(self, position_x, position_y, velocity_x, velocity_y, radius=5,
+                 canvas_width=900, canvas_height=600):
         self.position = np.array([position_x, position_y])
         self.velocity = np.array([velocity_x, velocity_y])
         self.radius = radius
@@ -24,12 +17,12 @@ class Ball:
         if (self.position[1] + self.radius >= self.canvas[1]) or (
                 self.position[1] - self.radius <= 0):
             self.velocity[1] *= -1
-    
+
     def check_for_goals(self):
         if (self.position[0] + self.radius >= self.canvas[0]):
             return [1, 0]
         if (self.position[0] - self.radius <= 0):
-            return [0, 1] 
+            return [0, 1]
         return [0, 0]
 
     def reset_position(self):
--- Environment/lib/game.py
+++ Environment/lib/game.py
@@ -1,6 +1,6 @@
-import numpy as np 
+import numpy as np
 from lib.ball import *
-from lib.paddle import * 
+from lib.paddle import *
 
 class Game:
     def __init__(self, total_game_time, refresh_time, canvas_width=900, canvas_height=600, paddle_type=Paddle, ball_type=Ball):
@@ -9,22 +9,38 @@ class Game:
         self.right_paddle = paddle_type(290, 1, 10)
         self.ball = ball_type(450, 300, 1, 1)
         self.time_remaining = total_game_time
+        self.refresh_time = refresh_time
         self.score = np.array([0, 0])
-    
-    def step(self):
-        self.ball.step()
-        self.left_paddle.step()
-        self.right_paddle.step()
+
+    def step(self, left_action=0, right_action=0):
+        self.step_components(left_action, right_action)
         if self.check_for_goals():
             self.reset_ball_position()
-    
+
+    def step_components(self, left_action, right_action):
+        self.ball.step()
+        self.left_paddle.step(left_action)
+        self.right_paddle.step(right_action)
+
+    def check_ball_paddle_collision(self):
+        self.check_left_ball_paddle_collision()
+        self.check_right_ball_paddle_collision()
+
+    def check_left_ball_paddle_collision(self):
+        if ((self.ball.position[0] - self.ball.radius <= self.left_paddle.thickness)
+           && (self.left_paddle.position[1] <= self.ball.position[1] <= self.left_paddle.position[1] + self.left_paddle.length)
+           && (self.ball.velocity[0] <= 0)):
+            self.ball.velocity[0] *= -1
+
+    def check_right_ball_paddle_collision(self):
+        if ((self.ball.position[0] + self.ball.radius <= self.right_paddle.position[0])
+           && (self.right_paddle.position[1] <= self.ball.position[1] <= self.right_paddle.position[1] + self.right_paddle.length)
+           && (self.ball.velocity[0] >= 0)):
+            self.ball.velocity[0] *= -1
+
     def reset_ball_position(self):
         self.ball.reset_position()
-    
+
     def check_for_goals(self):
         self.score += np.array(self.ball.check_for_goals())
         return sum(self.ball.check_for_goals())
-            
-            
-
-    
