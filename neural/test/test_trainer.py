5e51ae61bfbc1c29cceb185931908c4e8c20e33c
Cameron Whitehead <camjw119@gmail.com>
2018-11-15 19:30:43 +0000

    Added first test for trainer

--- neural/test/test_trainer.py
+++ neural/test/test_trainer.py
@@ -13,3 +13,8 @@ class TrainerTest(unittest.TestCase):
         networkMock = mock.Mock()
         competitorMock = mock.Mock()
         self.trainer = Trainer(gameMock, sessionMock, memoryMock, networkMock, competitorMock, 1, 0, 0.5, 0.9)
+
+    def test_computed_action(self):
+        self.trainer.network.single_prediction.return_value = [1,0,0]
+        state = { 'first': 0, 'second': 1 }
+        self.assertEqual(self.trainer.computed_action(state), -1)
