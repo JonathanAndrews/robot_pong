548c0f42f32dca41a5bb978e69b4ae2d6bcefc00
NadiaAiraf <aidanfaria@gmail.com>
2018-11-15 16:45:57 +0000

    created how to run the game in trainer class

--- neural/lib/game.py
+++ neural/lib/game.py
@@ -3,6 +3,8 @@ from lib.ball import *
 from lib.paddle import *
 
 class Game:
+    POSSIBLE_MOVES = [1, 0, -1]
+    
     def __init__(self, total_game_time, refresh_time, canvas_width=900,
                  canvas_height=600, paddle_type=Paddle, ball_type=Ball):
         self.canvas = np.array([canvas_width, canvas_height])
@@ -20,19 +22,6 @@ class Game:
         if self.check_for_goals():
             self.reset_ball_position()
         self.time_remaining -= 1
-        ai_inputs = {
-            'user-paddle-y': self.left_paddle.position,
-            'user-paddle-dy': self.left_paddle.speed,
-            'comp-paddle-y': self.right_paddle.position,
-            'comp-paddle-dy': self.right_paddle.speed,
-            'ball-position-x': self.ball.position_x,
-            'ball-position-y': self.ball.position_y,
-            'ball-velocity-dx': self.ball.velocity_x,
-            'ball-velocity-dy': self.ball.velocity_y,
-            'time-remaining': self.time_remaining,
-            'score': self.score[0] - self.score[1]
-        }
-        return ai_inputs
 
     def step_components(self, left_action, right_action):
         self.ball.step()
@@ -65,3 +54,33 @@ class Game:
     def is_game_over(self):
         if self.time_remaining <= 0:
             self.game_over = True
+
+    def return_user_state(self):
+        ai_inputs = {
+            'user-paddle-y': self.left_paddle.position,
+            'user-paddle-dy': self.left_paddle.speed,
+            'comp-paddle-y': self.right_paddle.position,
+            'comp-paddle-dy': self.right_paddle.speed,
+            'ball-position-x': self.ball.position_x,
+            'ball-position-y': self.ball.position_y,
+            'ball-velocity-dx': self.ball.velocity_x,
+            'ball-velocity-dy': self.ball.velocity_y,
+            'time-remaining': self.time_remaining,
+            'score': self.score[0] - self.score[1]
+        }
+        return ai_inputs
+
+    def return_competitor_state(self):
+        ai_inputs = {
+            'comp-paddle-y': self.left_paddle.position,
+            'comp-paddle-dy': self.left_paddle.speed,
+            'user-paddle-y': self.right_paddle.position,
+            'user-paddle-dy': self.right_paddle.speed,
+            'ball-position-x': self.canvas[0] - self.ball.position_x,
+            'ball-position-y': self.ball.position_y,
+            'ball-velocity-dx': -self.ball.velocity_x,
+            'ball-velocity-dy': self.ball.velocity_y,
+            'time-remaining': self.time_remaining,
+            'score': self.score[1] - self.score[0]
+        }
+        return ai_inputs
--- neural/lib/trainer.py
+++ neural/lib/trainer.py
@@ -4,14 +4,63 @@ import numpy as np
 class Trainer:
     '''The trainer of the neural network, using the game created'''
 
-    def __init__(self, game, session, memory, network, max_epsilon, min_epsilon, epsilon_decay, gamma):
+    def __init__(self, game, session, memory, network, competitor , max_epsilon, min_epsilon, epsilon_decay, gamma):
         self.game = game
         self.session = session
         self.memory = memory
         self.network = network
+        self.competitor = competitor
         self.max_epsilon = max_epsilon
         self.min_epsilon = min_epsilon
+        self.epsilon = max_epsilon
         self.epsilon_decay = epsilon_decay
         self.gamma = gamma
 
-    
+    def run_game(self):
+        user_state = self.game.return_user_state()
+        competitor_state = self.game.return_competitor_state()
+
+        while True:
+            user_action = self.user_action(user_state)
+            competitor_action = self.computed_action(competitor_state)
+            self.game.step(user_action, competitor_action)
+
+            new_user_state = self.return_user_state()
+            reward = self.calculate_reward(new_user_state)
+            done = (new_user_state['time-remaining'] <= 0)
+
+            self.add_sample(user_state, new_user_state, reward, user_action, done)
+            # update_epsilon
+            # replay_memories
+            # break_if_game_over
+
+
+
+
+
+
+
+
+    def user_action(self, state):
+        if random.random() < self.epsilon:
+            return random.choice(self.game.POSSIBLE_MOVES)
+        else:
+            return self.computed_action(state)
+
+    def computed_action(self, state):
+        state_values = np.array([value for value in state.values()])
+        return np.argmax(self.network.single_prediction(state_values, self.session)) - 1
+
+    def calculate_reward(self, state):
+        if state['time-remaining'] <= 0:
+            if state['score'] > 0:
+                return 1.0
+            else if state['score'] < 0:
+                return -1.0
+        return 0.0
+
+    def add_sample(self, user_state, new_user_state, reward, action, done):
+        if done:
+            self.memory.add_memory((user_state, None, reward, action))
+        else:
+            self.memory.add_memory((user_state, new_user_state, reward, action))
