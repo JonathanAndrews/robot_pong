3e104e3381f5748ef83cbda93f1f5999ece50231
Cameron Whitehead <camjw119@gmail.com>
2018-11-15 14:46:08 +0000

    Added blank method to network

--- neural/lib/network.py
+++ neural/lib/network.py
@@ -3,12 +3,13 @@ import tensorflow as tf
 class Network:
     '''The neural network. There are no ifs no buts just a neural network'''
 
-    def __init__(self, no_actions, no_inputs, hidden_layer_size=100, no_hidden_layers=3, keep_prob=0.9):
+    def __init__(self, no_actions, no_inputs, hidden_layer_size=100, no_hidden_layers=3, keep_prob=0.9, activation_function=tf.nn.tanh):
         self.no_actions = no_actions
         self.no_inputs = no_inputs
         self.hidden_layer_size = hidden_layer_size
         self.no_hidden_layers = no_hidden_layers
         self.keep_prob = keep_prob
+        self.activation_function = activation_function
 
         self.define_model()
 
@@ -36,14 +37,15 @@ class Network:
             hidden_biases[i] = tf.Variable(tf.zeros([self.hidden_layer_size]))
 
         layers = {}
-        layers[0] = tf.nn.tanh(tf.matmul(self.states, first_weights) + first_biases)
+        layers[0] = tf.nn.dropout(self.activation_function(tf.matmul(self.states, first_weights) + first_biases), self.dropout)
         for i in range(self.no_hidden_layers - 1):
-            layers[i + 1] = tf.nn.dropout(tf.nn.tanh(tf.matmul(layers[i], hidden_weights[i]) + hidden_biases[i]), self.dropout)
+            layers[i + 1] = tf.nn.dropout(self.activation_function(tf.matmul(layers[i], hidden_weights[i]) + hidden_biases[i]), self.dropout)
+
+        self.neural_output = tf.matmul(layers[self.no_hidden_layers - 1], last_weights) + last_biases
 
         self.saver = tf.train.Saver([first_weights, first_biases, last_weights, last_biases] +
                                     [hidden_weight for hidden_weight in hidden_weights.values()] +
                                     [hidden_bias for hidden_bias in hidden_biases.values()])
-        self.neural_output = tf.matmul(layers[self.no_hidden_layers - 1], last_weights) + last_biases
         self.loss = tf.losses.mean_squared_error(self.neural_output, self.q_values)
         self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
         self.variable_initializer = tf.global_variables_initializer()
@@ -66,3 +68,6 @@ class Network:
             self.q_values: outputs,
             self.dropout: self.keep_prob
         })
+
+    def load_network(self, filename):
+        pass
