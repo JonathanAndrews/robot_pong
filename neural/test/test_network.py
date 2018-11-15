41022dcb1d7440d4bf4d094b8fa8ab309c467f86
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-11-15 11:36:13 +0000

    tested define_variables and define_utilities methods

--- neural/test/test_network.py
+++ neural/test/test_network.py
@@ -12,3 +12,16 @@ class NetworkTest(unittest.TestCase):
         for parameter in [self.network.states, self.network.q_values, self.network.dropout]:
             self.assertNotEqual(None, parameter)
             
+    def test_define_variables(self):
+        self.network.saver = self.network.neural_output = None
+        self.network.define_model()
+        for parameter in [self.network.saver, self.network.neural_output]:
+            self.assertNotEqual(None, parameter)
+
+    def test_define_utilities(self):
+        self.network.loss = self.network.optimizer = self.network.variable_initializer = None
+        self.network.define_model()
+        for parameter in [self.network.loss, self.network.optimizer, self.network.variable_initializer]:
+            self.assertNotEqual(None, parameter)
+            
+
