eea1b31b2d790133d4e91b7795251d27309a5e8f
James Rodney <jlrodney95@gmail.com>
2018-11-19 11:44:43 +0000

    changed interface

--- public/src/canvasDisplay.js
+++ public/src/canvasDisplay.js
@@ -20,10 +20,10 @@ CanvasDisplay.prototype.drawLines = function drawLines() {
 };
 
 CanvasDisplay.prototype.drawScores = function (playerScore, aiScore) {
-  this.context.font = '20px Arial';
+  this.context.font = '22px Arial';
   this.context.strokeStyle = 'white';
   this.context.textAlign = 'center';
-  this.context.strokeText('ROBOT FACE PONG', 450, 40);
+  this.context.strokeText('R O B O T    F A C E    P O N G', 450, 40);
   this.context.fillText(playerScore + " - " + aiScore, 50, 30);
 };
 
@@ -32,80 +32,80 @@ CanvasDisplay.prototype.drawTime = function (interval) {
   this.context.font = '20px Arial';
   this.context.strokeStyle = 'white';
   this.context.textAlign = 'center';
-  this.context.fillText(interval, 800, 30);
+  this.context.fillText(interval+'s', 800, 30);
 };
 
 CanvasDisplay.prototype.drawRobot = function () {
   // robot's head
   this.context.beginPath();
-  this.context.rect(140, 200, 180, 180)
+  this.context.rect(140+450, 200, 180, 180)
   this.context.strokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   // robot's left eye
   this.context.beginPath();
-  this.context.arc(200, 250, 18, 0, Math.PI*2, true)
+  this.context.arc(200+450, 250, 18, 0, Math.PI*2, true)
   this.context.strokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   this.context.beginPath();
-  this.context.arc(200, 250, 6, 0, Math.PI*2, true)
+  this.context.arc(200+450, 250, 6, 0, Math.PI*2, true)
   this.context.fillStyle = "rgba(255,255,255)";
   this.context.fill();
 
   // robot's right eye
   this.context.beginPath();
-  this.context.arc(260, 250, 18, 0, Math.PI*2, true)
+  this.context.arc(260+450, 250, 18, 0, Math.PI*2, true)
   this.context.strokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   this.context.beginPath();
-  this.context.arc(260, 250, 6, 0, Math.PI*2, true)
+  this.context.arc(260+450, 250, 6, 0, Math.PI*2, true)
   this.context.fillStyle = "rgba(255,255,255)";
   this.context.fill();
 
   // antenna
   this.context.beginPath();
-  this.context.rect(225, 160, 10, 40);
+  this.context.rect(225+450, 160, 10, 40);
   this.context.stokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   this.context.beginPath();
-  this.context.arc(230, 140, 20, 0, Math.PI*2, true)
+  this.context.arc(230+450, 140, 20, 0, Math.PI*2, true)
   this.context.strokeStyle = "rgba(255,255,255)";
   this.context.stroke();
 
   // mouth
   this.context.beginPath();
-  this.context.rect(170, 300, 120, 40);
+  this.context.rect(170+450, 300, 120, 40);
   this.context.stokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   this.context.beginPath();
-  this.context.rect(170, 300, 120, 20);
+  this.context.rect(170+450, 300, 120, 20);
   this.context.stokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   this.context.beginPath();
-  this.context.rect(170, 300, 60, 40);
+  this.context.rect(170+450, 300, 60, 40);
   this.context.stokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   this.context.beginPath();
-  this.context.rect(170, 300, 30, 40);
+  this.context.rect(170+450, 300, 30, 40);
   this.context.stokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
 
   this.context.beginPath();
-  this.context.rect(230, 300, 30, 40);
+  this.context.rect(230+450, 300, 30, 40);
   this.context.stokeStyle = "rgba(255,255,255)";
   this.context.lineWidth = 3;
   this.context.stroke();
